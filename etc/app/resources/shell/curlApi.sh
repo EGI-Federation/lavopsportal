0355cbddc5c8fe50faef86df81c4ddbb9796e0fe
Cyril L'Orphelin <cyril.lorphelin@cc.in2p3.fr>
2022-03-14 17:25:41 +0100

    Integrate authorization into accouting API

--- etc/app/resources/shell/curlApi.sh
+++ etc/app/resources/shell/curlApi.sh
@@ -1,1 +1,1 @@
-curl $1 -X PATCH -H "accept: application/json" -H 'Content-Type: application/json' -d $2
+curl $1 -X PATCH -H "accept: application/json" -H 'Content-Type: application/json' -H $2 -d $3
